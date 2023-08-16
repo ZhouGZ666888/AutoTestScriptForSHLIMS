6cf4969cbe1976f1052b242d8bb59d387a10ff44
ZhouGZ666888 <342100511@qq.com>
2023-08-16 19:23:39 +0800

    APP-A

---
+++ PageElemens/app_a_ele.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/08/08
+# @Author  : guanzhong.zhou
+# @File    : AAP-A页面元素定位
+# -*-*************************************************************************************-*-
+"""
+AA-A首页列表元素定位
+"""
+#lims号检索文本录入框
+lims_input='.sample_receive_detail .el-table__body-wrapper tbody tr:nth-child(1) td:nth-child(4)'
+
+#查询按钮
+search_btn='.search-list .search-list-btns .el-button--primary'
